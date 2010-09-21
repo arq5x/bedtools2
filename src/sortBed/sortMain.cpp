a88aa54d3487b82baac33aba943a72ae77f6235c
Aaron <aaronquinlan@gmail.com>
2010-09-21 10:05:33 -0400

    sortBed assumes stdin.

--- src/sortBed/sortMain.cpp
+++ src/sortBed/sortMain.cpp
@@ -30,18 +30,17 @@ int main(int argc, char* argv[]) {
 	bool showHelp = false;
 
 	// input files
-	string bedFile;
-	bool haveBed = false;
+	string bedFile  = "stdin";
+	bool haveBed    = true;
 	int sortChoices = 0;
 
-	bool sortBySizeAsc = false;
-	bool sortBySizeDesc = false;
-	bool sortByChromThenSizeAsc = false;
-	bool sortByChromThenSizeDesc = false;
-	bool sortByChromThenScoreAsc = false;
+	bool sortBySizeAsc            = false;
+	bool sortBySizeDesc           = false;
+	bool sortByChromThenSizeAsc   = false;
+	bool sortByChromThenSizeDesc  = false;
+	bool sortByChromThenScoreAsc  = false;
 	bool sortByChromThenScoreDesc = false;
-	
-	if(argc <= 1) showHelp = true;
+
 	
 	for(int i = 1; i < argc; i++) {
 		int parameterLength = (int)strlen(argv[i]);
@@ -61,7 +60,6 @@ int main(int argc, char* argv[]) {
 
 		if(PARAMETER_CHECK("-i", 2, parameterLength)) {
 			if ((i+1) < argc) {
-				haveBed = true;
 				bedFile = argv[i + 1];
 				i++;
 			}
