cbeca9e3b0205f8805ed0314402f331342ceec12
Aaron <aaronquinlan@gmail.com>
2010-09-21 09:55:57 -0400

    complementBed assumes stdin.

--- src/complementBed/complementMain.cpp
+++ src/complementBed/complementMain.cpp
@@ -30,14 +30,11 @@ int main(int argc, char* argv[]) {
 	bool showHelp = false;
 
 	// input files
-	string bedFile;
+	string bedFile = "stdin";
 	string genomeFile;
 
-	bool haveBed = false;
+	bool haveBed = true;
 	bool haveGenome = false;
-
-	// check to see if we should print out some help
-	if(argc <= 1) showHelp = true;
 
 	for(int i = 1; i < argc; i++) {
 		int parameterLength = (int)strlen(argv[i]);
@@ -57,7 +54,6 @@ int main(int argc, char* argv[]) {
 
  		if(PARAMETER_CHECK("-i", 2, parameterLength)) {
 			if ((i+1) < argc) {
-				haveBed = true;
 				bedFile = argv[i + 1];
 				i++;
 			}
