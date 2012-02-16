f6b0439e3a5d61da33c9f0f66bb471093d20eadd
Aaron <aaronquinlan@gmail.com>
2012-02-16 10:59:55 -0500

    added tests for cluster

--- test/test.sh
+++ test/test.sh
@@ -1,49 +1,32 @@
 echo " Performing general tests:"
-cd general
-sh test-general.sh
-cd ..
+cd general; sh test-general.sh; cd ..
 
 echo " Testing bedtools bed12tobed6:"
-cd bed12tobed6
-sh test-bed12tobed6.sh
-cd ..
+cd bed12tobed6; sh test-bed12tobed6.sh; cd ..
 
 echo " Testing bedtools bamtobed:"
-cd bamtobed
-sh test-bamtobed.sh
-cd ..
+cd bamtobed; sh test-bamtobed.sh; cd ..
+
+echo " Testing bedtools cluster:"
+cd cluster; sh test-cluster.sh; cd ..
 
 echo " Testing bedtools coverage:"
-cd coverage
-sh test-coverage.sh
-cd ..
+cd coverage; sh test-coverage.sh; cd ..
 
 echo " Testing bedtools flank:"
-cd flank
-sh test-flank.sh
-cd ..
+cd flank; sh test-flank.sh; cd ..
 
 echo " Testing bedtools genomecov:"
-cd genomecov
-sh test-genomecov.sh
-cd ..
+cd genomecov; sh test-genomecov.sh; cd ..
 
 echo " Testing bedtools intersect:"
-cd intersect
-sh test-intersect.sh
-cd ..
+cd intersect; sh test-intersect.sh; cd ..
 
 echo " Testing bedtools map:"
-cd map
-sh test-map.sh
-cd ..
+cd map; sh test-map.sh; cd ..
 
 echo " Testing bedtools merge:"
-cd merge
-sh test-merge.sh
-cd ..
+cd merge; sh test-merge.sh; cd ..
 
 echo " Testing bedtools slop:"
-cd slop
-sh test-slop.sh
-cd ..
+cd slop; sh test-slop.sh; cd ..
