608e10c274bf87365769bc04d95e969109dd876e
Aaron <aaronquinlan@gmail.com>
2012-07-24 10:15:55 -0400

    add setup.py. for venv

--- docs/setup.py
+++ docs/setup.py
@@ -1,14 +1,6 @@
-import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='bedtools',
-    version="2.16.2",
-    description='A flexible toolset for genome arithmetic',
-    author='Aaron Quinlan',
-    author_email='arq5x@virginia.edu',
-    url='http://bedtools.googelcode.come',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False
-)
+    install_requires=['matplotlib', 'matplotlib.sphinxext'],
+    )
