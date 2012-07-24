ca360873125b18055e47adb9c35b7eefe46a45c5
Aaron <aaronquinlan@gmail.com>
2012-07-23 22:44:35 -0400

    trying to suppor mpl

--- docs/setup.py
+++ docs/setup.py
@@ -5,12 +5,10 @@ setup(
     name='bedtools',
     version="2.16.2",
     description='A flexible toolset for genome arithmetic',
-    author='Aaron Quinkan',
+    author='Aaron Quinlan',
     author_email='arq5x@virginia.edu',
     url='http://bedtools.googelcode.come',
     packages=find_packages(),
     include_package_data=True,
-    #install_requires=libraries,
-    zip_safe=False,
-    #test_suite='readthedocs.tests.rtd_tests.runtests'
+    zip_safe=False
 )
