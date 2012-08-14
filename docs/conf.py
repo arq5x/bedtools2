9186eb14525dc079f0c13da79266fb59a8fb90b0
Aaron <aaronquinlan@gmail.com>
2012-08-14 10:42:05 -0400

    try Mock()

--- docs/conf.py
+++ docs/conf.py
@@ -238,6 +238,6 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy', 'matplotlib', 'matplotlib.pyplot']
+MOCK_MODULES = ['numpy', 'matplotlib', 'matplotlib.pyplot', 'matplotlib.sphinxext']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
