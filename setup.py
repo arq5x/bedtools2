374609479631c32a214b4903b18c3e53b10ae3ff
Aaron <aaronquinlan@gmail.com>
2012-08-14 10:46:29 -0400

    no need for setup.py?

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from setuptools import setup
-
-setup(
-    name='bedtools',
-    install_requires=["numpy>=1.6.0", "matplotlib>=1.1.0"],
-    )
+# from setuptools import setup
+# 
+# setup(
+#     name='bedtools',
+#     install_requires=["numpy>=1.6.0", "matplotlib>=1.1.0"],
+#     )
