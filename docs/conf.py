a5914543bd54822c944c3621cbd872a25a2f924d
Aaron <aaronquinlan@gmail.com>
2012-12-27 10:38:00 -0500

    cleanup conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,6 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'rtd'
-#html_style = 'labibi.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -126,12 +125,6 @@ html_favicon = 'bedtools.png'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_style = 'labibi.css'
-
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
