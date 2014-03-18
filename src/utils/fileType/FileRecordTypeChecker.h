16a260b68c790f4b3f20d672adfe42358e8f19dd
John Marshall <john.marshall@sanger.ac.uk>
2014-03-18 10:48:05 +0000

    Miscellaneous portability / warning fixes
    Remove extra parentheses that clang thinks are there to prevent a
    different warning for "if (numConsumedByB = 0) /*assignment intended*/".
    Use 0 instead of NULL to initialise non-pointers.
    Only const *integral or enumeration* static data members can be portably
    initialised within the class definition.

--- src/utils/fileType/FileRecordTypeChecker.h
+++ src/utils/fileType/FileRecordTypeChecker.h
@@ -107,8 +107,6 @@ private:
 	map<RECORD_TYPE, bool> _hasScore;
 	map<RECORD_TYPE, bool> _hasStrand;
 
-	//this will be used in determining whether we are looking at a binary or text file.
-	static const float PERCENTAGE_PRINTABLE = .9;
 	bool isBinaryBuffer(const char *buffer, size_t len);
 	bool isBAM(const char *buffer);
 	bool handleTextFormat(const char *buffer, size_t len);
