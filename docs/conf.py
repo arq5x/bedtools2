3dff8cadd8fc10ba1bee9d407c0758901e0f21ca
arq5x <arq5x@virginia.edu>
2014-09-18 10:51:45 -0400

    [DOC] remove sticky note from docs. general hatred.

--- docs/conf.py
+++ docs/conf.py
@@ -113,7 +113,7 @@ html_short_title = project + " v" + release
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'bedtools.png'
+html_logo = 'bedtools.swiss.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
