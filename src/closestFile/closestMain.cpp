41193721bb5df1b421919873d81b4764d4bb8dde
arq5x <arq5x@virginia.edu>
2015-01-01 20:47:22 -0500

    update help messages.

--- src/closestFile/closestMain.cpp
+++ src/closestFile/closestMain.cpp
@@ -91,6 +91,11 @@ void closest_help(void) {
     cerr                        << "\t\t- \"each\"    Report closest records for each database (default)." << endl;
     cerr                        << "\t\t- \"all\"  Report closest records among all databases." << endl << endl;
 
+    cerr << "\t-names\t"        << "When using multiple databases (-b), provide an alias for each that" << endl;
+    cerr                        <<"\t\twill appear instead of a fileId when also printing the DB record." << endl << endl;
+
+    cerr << "\t-filenames"      << "\tWhen using multiple databases (-b), show each complete filename" << endl;
+    cerr                        <<"\t\t\tinstead of a fileId when also printing the DB record." << endl << endl;
 
     cerr << "\t-N\t"            << "Require that the query and the closest hit have different names." << endl;
     cerr                        << "\t\tFor BED, the 4th column is compared." << endl << endl;
