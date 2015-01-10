3398f726529b5512bd39e05062192c2246598d72
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2015-01-10 19:48:02 +0100

    simple algorithm when n< FOPEN_MAX

--- test/split/test-split.sh
+++ test/split/test-split.sh
@@ -15,4 +15,11 @@ rm -f _tmp.*.bed
 ${BT} split -i tmp.bed -p _tmp -n 50 -a simple > _tmp.simple.tsv
 cat _tmp.simple.tsv
 
+echo "    slpit.simple...\c"
+rm -f _tmp.*.bed
+${BT} split -i tmp.bed -p _tmp -n 1000 -a simple > _tmp.simple.tsv
+cat _tmp.simple.tsv
+
+
+
 rm -f _tmp.*.bed jeter.bed tmp.bed  _tmp.simple.tsv  _tmp.size.tsv
