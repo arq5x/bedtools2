83c6faa97d06fa0d57391a3e4f4292e01f2d00b2
lindenb <plindenbaum@yahoo.fr>
2015-04-17 17:44:17 +0200

    sort BED on faidx file

--- src/sortBed/sortBed.cpp
+++ src/sortBed/sortBed.cpp
@@ -9,13 +9,14 @@
 
   Licenced under the GNU General Public License 2.0 license.
 ******************************************************************************/
+#include <set>
 #include "lineFileUtilities.h"
 #include "sortBed.h"
 
 //
 // Constructor
 //
-BedSort::BedSort(string &bedFile, bool printHeader) {
+BedSort::BedSort(string &bedFile, bool printHeader,string &faidxFile):_faidxFile(faidxFile) {
     _bedFile = bedFile;
     _bed = new BedFile(bedFile);
     
@@ -46,6 +47,72 @@ void BedSort::SortBed() {
         }
     }
 }
+
+void BedSort::SortBedOnFaidx()
+		{
+		set<string> all_chromosomes;
+		if(_faidxFile.empty())
+				{
+				cerr << "[sortBed] File for fasta index undefined." << endl;
+				exit(EXIT_FAILURE);
+				}
+		/* read FAIDX file */
+		ifstream faidx(_faidxFile.c_str(),ios::in);
+		if(!faidx.is_open()) {
+				cerr << "Cannot open \""<< _faidxFile << "\""
+						<< strerror(errno)
+						<< endl;
+				exit(EXIT_FAILURE);
+				}
+		string line;
+		while(getline(faidx,line,'\n')) {
+				if(line.empty()) continue;
+				string::size_type tab= line.find('\t');
+				if(tab!=string::npos) {
+						line.erase(tab);
+						}
+				if(all_chromosomes.find(line) != all_chromosomes.end()) {
+						cerr  << "Chromosome \"" << line
+									<<"\" defined twice in "
+									<< _faidxFile
+									<< endl;
+						exit(EXIT_FAILURE);
+						}
+				_tid2chrom[_tid2chrom.size()]=line;
+				all_chromosomes.insert(line);
+				}
+		faidx.close();
+		/** end read FAIDX */
+		
+		//check BED chromosomes
+		for (masterBedMapNoBin::iterator m = _bed->bedMapNoBin.begin(); m != _bed->bedMapNoBin.end(); ++m) {
+					if( all_chromosomes.find(m->first) ==  all_chromosomes.end()) {
+						cerr  << "Chromosome \"" << m->first
+									<<"\" undefined in "
+									<< _faidxFile
+									<< endl;
+						exit(EXIT_FAILURE);
+						}
+				}	
+		
+		
+		//loop over each chromosome using the faidx index
+		for(size_t tid=0; tid <_tid2chrom.size(); ++tid)
+			{
+			string chrom = _tid2chrom[tid];
+			masterBedMapNoBin::iterator m = _bed->bedMapNoBin.find(chrom);
+			
+			if( m == _bed->bedMapNoBin.end() ) continue; //this chromosome is not present in BED
+			
+		    // bedList is already sorted by start position.
+		    vector<BED> bedList = m->second;
+
+		    for (unsigned int i = 0; i < bedList.size(); ++i) {
+		        _bed->reportBedNewLine(bedList[i]);
+			  }
+			}
+		
+		}
 
 
 void BedSort::SortBedBySizeAsc() {
