5e017b63155f8a7f3e43a2e551bcfc535cd5bf0a
Gaik Tamazian <gaik.tamazian@gmail.com>
2015-05-18 15:03:58 +0300

    Possible overflows eliminated

--- src/slopBed/slopBed.cpp
+++ src/slopBed/slopBed.cpp
@@ -41,7 +41,7 @@ BedSlop::~BedSlop(void) {
 void BedSlop::SlopBed() {
 
     BED bedEntry;     // used to store the current BED line from the BED file.
-    float l, r;
+    float l = _leftSlop, r = _rightSlop;
 
     _bed->Open();
     // report header first if asked.
@@ -54,17 +54,16 @@ void BedSlop::SlopBed() {
                 AddSlop(bedEntry);
             }
             else {
-	        l = _leftSlop;	
-                _leftSlop  = _leftSlop * (float)bedEntry.size();
-	        r = _rightSlop;	
-                _rightSlop = _rightSlop * (float)bedEntry.size();
+                _leftSlop  = l * bedEntry.size();
+                _rightSlop = r * bedEntry.size();
                 AddSlop(bedEntry);
-	        _rightSlop = r;
-	        _leftSlop = l;
             }
             _bed->reportBedNewLine(bedEntry);
         }
     }
+	_leftSlop = l;
+	_rightSlop = r;
+
     _bed->Close();
 }
 
@@ -74,34 +73,26 @@ void BedSlop::AddSlop(BED &bed) {
     // special handling if the BED entry is on the negative
     // strand and the user cares about strandedness.
     CHRPOS chromSize = (CHRPOS)_genome->getChromSize(bed.chrom);
+	
+	CHRPOS leftShift, rightShift;
 
     if ( (_forceStrand) && (bed.strand == "-") ) {
-        if ( ((int)bed.start - (int)_rightSlop) >= 0 )
-        {
-            bed.start = bed.start - (int)_rightSlop;
-        }
-        else
-            bed.start = 0;
-
-        if ( ((int)bed.end + (int)_leftSlop) <= chromSize )
-            bed.end = bed.end + (int)_leftSlop;
-        else
-            bed.end = chromSize;
+        leftShift = (CHRPOS)_rightSlop;
+        rightShift = (CHRPOS)_leftSlop;
+    } else {
+        leftShift = (CHRPOS)_leftSlop;
+        rightShift = (CHRPOS)_rightSlop;
     }
-    else {
-    	if ( ((int)bed.start - (int)_leftSlop) >= 0 )
-            bed.start = bed.start - (int)_leftSlop;
-        else
-            bed.start = 0;
-        if ( ((int)bed.end + (int)_rightSlop) <= chromSize )
-            bed.end = bed.end + (int)_rightSlop;
-        else
-            // if the _rightSlop is negative and pushes bed.end to be < 0, set to 1
-            if ( ((int)bed.end + (int)_rightSlop) <= 0 )
-                bed.end = 1;
-            else
-                bed.end = chromSize;
-    }
+    
+    if (bed.start >= leftShift)
+        bed.start -= leftShift;
+    else
+        bed.start = 0;
+    
+    if ((rightShift > chromSize) || (bed.end > chromSize - rightShift))
+        bed.end = chromSize;
+    else
+        bed.end += rightShift;
 }
 
 
