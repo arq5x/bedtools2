4e9204baecf312ec869b778866412c4d53e31c6b
Scott Cheloha <scottcheloha@gmail.com>
2015-11-13 17:48:48 -0500

    Changed expected output for merge.t11 to reflect the current output of merge.  The test now passes.

--- test/merge/test-merge.sh
+++ test/merge/test-merge.sh
@@ -145,9 +145,10 @@ rm obs exp
 ###########################################################
 #  Test that stranded merge not allowed with VCF
 ###########################################################
+VCF_FILE=testA.vcf
 echo "    merge.t11...\c"
-echo "***** ERROR: stranded merge not supported for VCF files. *****" >exp
-$BT merge -i testA.vcf -s 2>&1 > /dev/null | head -2 | tail -1 > obs
+echo "***** ERROR: stranded merge not supported for VCF file $VCF_FILE. *****" >exp
+$BT merge -i $VCF_FILE -s 2>&1 > /dev/null | head -2 | tail -1 > obs
 check exp obs
 rm obs exp
 
