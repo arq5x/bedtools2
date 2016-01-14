cffff8b08386195e3941ab0a1f5e29ac47819ca7
arq5x <arq5x@virginia.edu>
2016-01-13 22:54:28 -0700

    add -bedOut option to getfasta

--- src/fastaFromBed/fastaFromBed.cpp
+++ src/fastaFromBed/fastaFromBed.cpp
@@ -17,12 +17,14 @@
 Bed2Fa::Bed2Fa(bool useName, const string &dbFile, 
                const string &bedFile, const string &fastaOutFile, 
                bool useFasta, bool useStrand, 
-               bool useBlocks, bool useFullHeader) :
+               bool useBlocks, bool useFullHeader,
+               bool useBedOut) :
     _useName(useName),
     _dbFile(dbFile),
     _bedFile(bedFile),
     _fastaOutFile(fastaOutFile),
     _useFasta(useFasta),
+    _useBedOut(useBedOut),
     _useStrand(useStrand),
     _useBlocks(useBlocks),
     _useFullHeader(useFullHeader)
@@ -66,41 +68,47 @@ void Bed2Fa::ReportDNA(const BED &bed, string &dna) {
     if ((_useStrand == true) && (bed.strand == "-"))
         reverseComplement(dna);
 
-    if (!(_useName)) {
-        if (_useFasta == true) {
-            if (_useStrand == true)
-                *_faOut << ">" << bed.chrom << ":" 
-                        << bed.start << "-" << bed.end   
-                        << "(" << bed.strand << ")" 
-                        << endl 
-                        << dna
-                        << endl;
-            else
-                *_faOut << ">" << bed.chrom << ":" 
-                        << bed.start << "-" << bed.end 
-                        << endl 
-                        << dna
-                        << endl;
-        }
-        else {
-            if (_useStrand == true)
-                *_faOut << bed.chrom << ":" 
-                        << bed.start << "-" << bed.end
-                        << "(" << bed.strand << ")" << "\t" 
-                        << dna 
-                        << endl;
-            else
-                *_faOut << bed.chrom << ":" 
-                        << bed.start << "-" << bed.end << "\t" 
-                        << dna 
-                        << endl;
-        }
+    if (_useBedOut) {
+        _bed->reportBedTab(bed);
+        cout << dna << endl;
     }
     else {
-        if (_useFasta == true)
-            *_faOut << ">" << bed.name << endl << dna << endl;
-        else
-            *_faOut << bed.name << "\t" << dna << endl;
+        if (!(_useName)) {
+            if (_useFasta == true) {
+                if (_useStrand == true)
+                    *_faOut << ">" << bed.chrom << ":" 
+                            << bed.start << "-" << bed.end   
+                            << "(" << bed.strand << ")" 
+                            << endl 
+                            << dna
+                            << endl;
+                else
+                    *_faOut << ">" << bed.chrom << ":" 
+                            << bed.start << "-" << bed.end 
+                            << endl 
+                            << dna
+                            << endl;
+            }
+            else {
+                if (_useStrand == true)
+                    *_faOut << bed.chrom << ":" 
+                            << bed.start << "-" << bed.end
+                            << "(" << bed.strand << ")" << "\t" 
+                            << dna 
+                            << endl;
+                else
+                    *_faOut << bed.chrom << ":" 
+                            << bed.start << "-" << bed.end << "\t" 
+                            << dna 
+                            << endl;
+            }
+        }
+        else {
+            if (_useFasta == true)
+                *_faOut << ">" << bed.name << endl << dna << endl;
+            else
+                *_faOut << bed.name << "\t" << dna << endl;
+        }
     }
 }
 
