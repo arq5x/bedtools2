c0b7d934cc61ad6c83eb3d99374263e7ec51722d
Jonathan Chang <jchang641@gmail.com>
2017-09-24 23:14:33 -0700

    strandqueue: de-const for stricter Xcode 9 rules

--- src/utils/FileRecordTools/Records/StrandQueue.h
+++ src/utils/FileRecordTools/Records/StrandQueue.h
@@ -20,17 +20,17 @@ public:
 	StrandQueue();
 	~StrandQueue();
 
-	Record * top() const;
+	Record * top();
 	void pop();
-	Record * top(Record::strandType strand) const;
-	void pop(Record::strandType strand) const;
+	Record * top(Record::strandType strand);
+	void pop(Record::strandType strand);
 	void push(Record *record);
-	size_t size() const;
-	bool empty() const;
+	size_t size();
+	bool empty();
 
 private:
 //	static RecordPtrSortFunctor _recSortFunctor;
-	typedef priority_queue<Record *, vector<const Record *>, RecordPtrSortDescFunctor > queueType;
+	typedef priority_queue<Record *, vector<Record *>, RecordPtrSortDescFunctor > queueType;
 	vector<queueType *> _queues;
 	static const int NUM_QUEUES = 3;
 
@@ -39,7 +39,7 @@ private:
 	//do, so we'll use a suggestion found in a forum, and put the enum values into a vector.
 	vector<Record::strandType> _strandIdxs;
 
-	int getMinIdx() const; //will return the idx of queue with the current min val.
+	int getMinIdx(); //will return the idx of queue with the current min val.
 
 };
 
