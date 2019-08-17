65207e208c2fab6cd81b2e8259f77886d3e94329
arq5x <aaronquinlan@gmail.com>
2019-08-17 09:36:07 -0600

    issue #730. Can't use -s with -n in makewindows

--- src/windowMaker/windowMakerMain.cpp
+++ src/windowMaker/windowMakerMain.cpp
@@ -143,6 +143,10 @@ int windowmaker_main(int argc, char* argv[]) {
         cerr << endl << "*****" << endl << "*****ERROR: Can't combine -w (window size) and -n (number of windows). Please use one or the other. " << endl << "*****" << endl;
         showHelp = true;
     }
+    if (haveStep && haveCount) {
+        cerr << endl << "*****" << endl << "*****ERROR: Can't combine -s (step size) and -n (number of windows). Please use one or the other. " << endl << "*****" << endl;
+        showHelp = true;
+    }
     if (step <= 0 && !haveBed && !haveCount && haveGenome) {
         cerr << endl << "*****" << endl << "*****ERROR: The step (-s) option must be greater than zero. " << endl << "*****" << endl;
         showHelp = true;
