179ded2768274021990d012055a864588ecc08b2
Steffen Möller <steffen_moeller@gmx.de>
2020-01-23 11:42:23 +0100

    Fix failing test new intersect #78
    Substituted blanks with tabs, added  --no-PG  to samtools view, all suggested by @jmarshall  in response to issue https://github.com/arq5x/bedtools2/issues/814

--- test/intersect/new_test-intersect.sh
+++ test/intersect/new_test-intersect.sh
@@ -975,8 +975,8 @@ rm exp obs
 #  Test intersect preserve the text header in bam
 ############################################################
 echo -e "    intersect.new.t78...\c"
-echo -e "@HD     VN:1.5  SO:coordinate" > exp
-echo "@HD     VN:1.5  SO:coordinate" | samtools view -b  | $BT intersect -a /dev/stdin -b b.bed | samtools view -H >obs
+echo -e "@HD	VN:1.5	SO:coordinate" > exp
+echo "@HD	VN:1.5	SO:coordinate" | samtools view --no-PG -b  | $BT intersect -a /dev/stdin -b b.bed | samtools view --no-PG -H >obs
 check exp obs
 rm exp obs
 [[ $FAILURES -eq 0 ]] || exit 1;
